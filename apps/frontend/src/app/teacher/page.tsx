818519b5793f524c45d0b50b913804f3c8e8ccf0
pureverdene93 <pureverdenej93@gmail.com>
2026-03-24 16:59:29 +0800

    cicd fix

--- apps/frontend/src/app/teacher/page.tsx
+++ apps/frontend/src/app/teacher/page.tsx
@@ -3,7 +3,8 @@ import TeacherDashboard from "./components/teacher-dashboard";
 
 export default function TeacherPage() {
   return (
-    <div className="min-h-screen bg-[#edf2f8] text-[#1f2a44]">
+    <div className="min-h-screen bg-[#edf2f8] text-[#1f2a44]"> 
+    hfiuehrgiuhreuh
       <Header />
       <TeacherDashboard />
     </div>
